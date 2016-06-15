99709f157e7324a8538dd1d0aa51aa31baacc1dd
Antonio Sejas <antonio@sejas.es>
2016-06-15 19:07:44 +0100

    place and photo classes basic

---
+++ tourist-planner/tourist-planner/PhotoFlickr.swift
@@ -0,0 +1,21 @@
+//
+//  PhotoFlikr.swift
+//  tourist-planner
+//
+//  Created by Antonio Sejas on 15/6/16.
+//  Copyright © 2016 Antonio Sejas. All rights reserved.
+//
+
+import UIKit
+
+class PhotoFlickr: NSObject {
+
+    var place: Place?
+    var url: String?
+    var id: String?
+    
+
+    
+    
+    
+}
---
+++ tourist-planner/tourist-planner/Place.swift
@@ -0,0 +1,17 @@
+//
+//  Place.swift
+//  tourist-planner
+//
+//  Created by Antonio Sejas on 15/6/16.
+//  Copyright © 2016 Antonio Sejas. All rights reserved.
+//
+
+import UIKit
+
+class Place: NSObject {
+    var latitude: Double
+    var longitude: Double
+    var photos:[PhotoFlickr]?
+    
+    
+}
